488d0b00a00612f009388d7cce8d892c745d67d3
Jasper Cacbay <jasper.cacbay@gmail.com>
2018-07-22 01:32:52 +0800

    fix(package): add mongodb dependency

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 var createError = require('http-errors');
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongodb = require('mongodb');
+var ObjectID = mongodb.ObjectID;
+
+var TODO_COLLECTION = "todos";
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var apiRouter = require('./routes/api.route');
 
 var bluebird = require('bluebird');
+
+var db;
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/todoapp", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
 
 var mongoose = require('mongoose');
 mongoose.Promise = bluebird;
@@ -28,6 +51,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
